5899954d884f7027e5df58b676ba58cecaa3e613
artemkha <khaapt@gmail.com>
2018-06-02 17:20:44 +0300

    App > react-navigation

--- App.js
+++ App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { StackNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation';
 
 class App extends Component {
+  static navigationOptions = {
+    title: 'Home',
+    headerStyle: {
+      backgroundColor: '#373142'
+    },
+    headerTitleStyle: {
+      color: '#FFF'
+    }
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Text style={styles.welcome}>It's React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
       </View>
     );
   }
@@ -24,22 +28,22 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 10
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
-  },
+    marginBottom: 5
+  }
 });
 
-export default StackNavigator({
+export default createStackNavigator({
   Home: {
-    screen: App,
-  },
+    screen: App
+  }
 });
