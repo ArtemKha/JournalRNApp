84f73f045406fbd27bc7d6d5a6898be6fa95a651
artemkha <khaapt@gmail.com>
2018-06-03 17:08:12 +0300

    Apollo setup

--- App.js
+++ App.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 import { Text, View, Button } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
+import { ApolloProvider } from 'react-apollo';
+import ApolloClient from 'apollo-boost';
 import navigationStyles from './styles/navigationStyles';
 import styles from './styles';
 import Post from './src/Post';
+import Posts from './src/Posts';
+
+// graph.cool/Journal dataBase
+const client = new ApolloClient({
+  uri: 'https://api.graph.cool/simple/v1/cjhxizhdn57rh0194ubnrb6h3'
+});
 
 class App extends Component {
   static navigationOptions = {
@@ -17,10 +25,14 @@ class App extends Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>It's React Native!</Text>
-        <Button onPress={this.goToPost} title="Go to post page" />
-      </View>
+      <ApolloProvider client={client}>
+        <View style={styles.container}>
+          <Text style={styles.welcome}>It's React Native!</Text>
+          <Text style={styles.welcome}>My Apollo app 🚀</Text>
+          <Posts />
+          <Button onPress={this.goToPost} title="Go to post page" />
+        </View>
+      </ApolloProvider>
     );
   }
 }
